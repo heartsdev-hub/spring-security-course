8596881ffb47fbd34bc17902c520cad81a02824a
Jack Perez <heartsdev2399@gmail.com>
2026-08-04 19:18:36 -0500

    Inicio de spring security

---
+++ src/main/java/com/spring_security/course_spring_security/CourseSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.spring_security.course_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_security/course_spring_security/CourseSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security.course_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
